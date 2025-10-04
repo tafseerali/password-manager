b1adca9db86fce74f372d10332df8030bdf00064
= <=>
2025-10-04 10:23:27 +0500

    Fixed Responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'eighty': '880px',             // ≥880px
+        'max-eighty': { max: '879px' }, // <880px
+        'six': '600px',
+        'max-six': {max: '600px'}
+      }
+    },
   },
   plugins: [],
 }
